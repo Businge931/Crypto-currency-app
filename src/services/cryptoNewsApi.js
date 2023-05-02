7fb27181645db8efabb73012ebf7f41180f405f5
Businge931 <jimmybusinge9@gmail.com>
2023-05-02 03:02:06 +0300

    feat:create endpoints for fetching data with redux

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "X-BingApis-SDK": "true",
+  "X-RapidAPI-Key": "b7ed264256msh6ec79a9b75ac8c6p1f68d2jsn92fdc3041883",
+  "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
